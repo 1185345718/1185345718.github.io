ab5d48ebda5f38f6a6b2c7b1789cb2dae6be7770
1185345718 <1185345718@qq.com>
2016-11-13 16:39:34 +0800

    哈哈

---
+++ xiangmu/wangyiyun/user.sql
@@ -0,0 +1,7 @@
+create table user(
+	phone varchar(11) primary key not null,
+	password varchar(20) not null
+);
+
+insert into user values('15871613695','zeng0304');
+insert into user values('15672483726','dingding');
